1bdd5d44d948d81c3a139a3c672cf02501e0755e
SurielRuano <fsuriel33@gmail.com>
2016-09-14 10:50:24 -0500

    Cambio de sqlite por Postgres

--- unmomento/settings.py
+++ unmomento/settings.py
@@ -77,9 +77,18 @@ WSGI_APPLICATION = 'unmomento.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'unmomento',
+        'USER': 'usrunmomento',
+        'PASSWORD': 'unmomento',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
