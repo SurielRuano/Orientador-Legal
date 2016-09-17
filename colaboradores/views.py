a3be31d6019bcf254c3dcbbb26b4ffbbc85a9871
SurielRuano <fsuriel33@gmail.com>
2016-09-17 08:56:21 -0500

    template tags, detalles, formulario de solicitud

--- colaboradores/views.py
+++ colaboradores/views.py
@@ -13,8 +13,28 @@ class Solicitud_colaboracion(View):
 	def get(self,request):
 
 		template_name = 'registro/registro.html'
+		
 		form = SolicitudColaboracionForm()
 
 		context = {'form':form}
 
 		return render (request,template_name,context)
+
+	def post(self,request):
+		template_name = "registro/registro.html"
+		template_name_success = 'registro/solicitud_exitosa.html'
+
+		new_solicit_form = SolicitudColaboracionForm(request.POST)
+
+		if new_solicit_form.is_valid():
+
+			new_solicit = new_solicit_form.save(commit=False)
+			new_solicit.save()
+			return render(request,template_name_success)
+
+		else:
+			context = {
+
+			'form': new_solicit_form
+			}
+			return render(request,template_name,context);
--- plataforma/templatetags/categorias_inclusion_tag.py
+++ plataforma/templatetags/categorias_inclusion_tag.py
@@ -1,6 +1,6 @@
 from django import template
 register = template.Library()
-from plataforma.models import Categoria
+from plataforma.models import Categoria, Articulo
 
 
 
@@ -19,5 +19,23 @@ def show_categorias_footer(count=6):
 	categorias= Categoria.objects.all().order_by('-nombre_cat')[:count]
 	return {
 	'categorias':categorias
+	}
+
+
+## tag que recibe una variable y renderiza un pequeno template html
+@register.inclusion_tag('template_tags/articulos_recientes.html')
+def show_articulos_footer(count=6):
+	articulos= Articulo.objects.all().order_by('-fecha')[:count]
+	return {
+	'articulos':articulos
+	}
+
+
+## tag que recibe una variable y renderiza un pequeno template html
+@register.inclusion_tag('template_tags/articulos_sidebar.html')
+def show_articulos_sidebar(count=6):
+	articulos= Articulo.objects.all().order_by('-fecha')[:count]
+	return {
+	'articulos':articulos
 	}
 
